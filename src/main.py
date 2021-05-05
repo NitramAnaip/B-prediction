142802e157d37440e7041970474acd1cf5b1946d
NitramAnaip <martin.piana@eleves.enpc.fr>
2021-05-05 14:16:42 +0200

    Fixed issue in scaled volume input but still having problems with identical output

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import time
 from sklearn import metrics
 
 import torch
@@ -8,16 +9,15 @@ import torch.nn as nn
 from keras.utils import to_categorical
 
 from utils import preprocess, split_multi_seq, get_unblaced_freq
-from model import LSTM
+from model import LSTM, GRU
 
 
 #opening cvs
-df = pd.read_csv("Binance_BTCUSDT_1h.csv")
-
-df, num_classes = preprocess(df)
+df = pd.read_csv("Binance_LINKUSDT_1h.csv")
+df_btc = pd.read_csv("Binance_BTCUSDT_1h.csv")
+df, num_classes = preprocess(df, df_btc)
 
 labels = [ 0, 1, 2]
-get_unblaced_freq(df, labels)
 
 # How many periods looking back to train
 n_per_in  = 30
@@ -31,24 +31,25 @@ n_per_out = 1
 
 close = np.array(df['groups'])
 vol = list(df['Volume USDT'])
-evolution_group = np.array(to_categorical(close,num_classes), dtype=np.float32)
+evolution_group = list(close)#np.array(to_categorical(close,num_classes), dtype=np.float32)
 split = list(df['split'])
 close_evolution = list(df["evolution"])
+btc_evol = list(df["btc_evol"])
 
 
 
-X, y = split_multi_seq(close_evolution, vol, split, evolution_group, n_per_in, n_per_out)
+X, y = split_multi_seq(close_evolution, vol, split, btc_evol, evolution_group, n_per_in, n_per_out)
 
 
 
 
-y = y.reshape(y.shape[0], y.shape[2])
+y = y.reshape(y.shape[0])
 #y = tf.convert_to_tensor(y)
 # Reshaping the X variable from 2D to 3D
 #X = tf.convert_to_tensor(X)
 # Reshaping the X variable from 2D to 3D
 
-
+print(y)
 
 
 X_val, X = X[:int(X.shape[0]/10)], X[int(X.shape[0]/10):]
@@ -60,23 +61,25 @@ y_train = torch.from_numpy(y).type(torch.Tensor)
 X_val = torch.from_numpy(X_val).type(torch.Tensor)
 
 
-input_dim = 3
+input_dim = 4
 hidden_dim = 32
 num_layers = 6
 output_dim = 3
-num_epochs = 40
+num_epochs = 5
 
 model = LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
-criterion = torch.nn.MSELoss(reduction='mean')
-optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+criterion = torch.nn.CrossEntropyLoss(weight = torch.from_numpy(np.array([0.3, 0.4, 0.3])).type(torch.Tensor))
+optimiser = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 
-import time
+
 hist = np.zeros(num_epochs)
 start_time = time.time()
 lstm = []
 for t in range(num_epochs):
     y_train_pred = model(x_train)
-    loss = criterion(y_train_pred, y_train)
+
+    #print(y_train_pred.shape())
+    loss = criterion(y_train_pred, y_train.type(torch.LongTensor))
     print("Epoch ", t, "MSE: ", loss.item())
     hist[t] = loss.item()
     optimiser.zero_grad()
@@ -86,6 +89,7 @@ for t in range(num_epochs):
 training_time = time.time()-start_time
 print("Training time: {}".format(training_time))
 
+print(x_train)
 print(y_train_pred)
 
 
@@ -102,4 +106,3 @@ if y_pred is not None:
         TP = a[i][1][1]
         FP = a[i][0][1]
         print("cat {} TP/(TP+FP): {}".format(i, TP/(FP+TP)))
-    
--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,7 @@ from keras.layers import LSTM, Dense, Dropout
 
 
 
-def preprocess(df):
+def preprocess(df, df_btc):
 
     df_new = pd.DataFrame({"unix": [], "open": [], "high": [], "low": [], "close": [], "Volume USDT": []})
 
@@ -32,21 +32,28 @@ def preprocess(df):
 
     #Creating the % evolution
     evol = []
+    evol_btc = []
     for i in range(df.shape[0]-1):
         evol.append((df['close'][i]-df['close'][i+1])/df['close'][i+1])
+        evol_btc.append((df_btc['close'][i]-df_btc['close'][i+1])/df_btc['close'][i+1])
+        
 
 
     evol.append(0)
+    evol_btc.append(0)
     df['evolution'] = evol
 
     df["split"] = (df["high"] - df["low"])/df["low"]
+    df_btc["split"] = (df_btc["high"] - df_btc["low"])/df_btc["low"]
+
     labels = [ 0, 1, 2]
-    df["groups"] = pd.cut(df["evolution"], bins=[-100,-0.005,0.005, 100], labels = labels)
-    num_classes = len(labels)
+    num_classes=len(labels)
+    df["groups"] = pd.cut(df["evolution"], bins=[-100,-0.01,0.01, 100], labels = labels)
+    df["btc_evol"] = evol_btc
+
 
     #Choice of features
-    df = df.set_index("unix")[["evolution", 'groups', 'Volume USDT', "split"]].head(32000)
-    
+    df = df.set_index("unix")[["evolution", 'groups', 'Volume USDT', "split", "btc_evol"]].head(32000)
     return df, num_classes
 
 def get_unblaced_freq(df, labels):
@@ -54,7 +61,7 @@ def get_unblaced_freq(df, labels):
         print(df[df["groups"]==label].shape[0])
 
 
-def split_multi_seq(close_evolution, volume, split, evolution_group, n_steps_in, n_steps_out):
+def split_multi_seq(close_evolution, volume, split, btc_evol, evolution_group, n_steps_in, n_steps_out):
     """
     Here seq is of the form [[prices], [volumes], ...]
     """
@@ -72,11 +79,13 @@ def split_multi_seq(close_evolution, volume, split, evolution_group, n_steps_in,
         #print(volume)
         scaled_vol = scaled_vol.reshape(-1, 1)
         
-        scaler.fit(scaled_vol) #I'm scaling the volume only over the past 30 hr or so: It doen't 
-                                    #make sense to scale it over the past 5 years
+        scaler.fit(scaled_vol) 
         scaled_vol = scaler.transform(scaled_vol)
         
         scaled_vol = list(scaled_vol.reshape(scaled_vol.shape[0]))
+        scaled_vol = scaled_vol[-n_steps_in:]
+
+
         #print(volume)
         
         # This is a trick I'm trying: I can't have my training data of the for [ [0,0,1], [volume]]
@@ -86,7 +95,7 @@ def split_multi_seq(close_evolution, volume, split, evolution_group, n_steps_in,
             inputs = [close_evolution[k]] #evolution
             inputs.append(scaled_vol[k-i])
             inputs.append(split[k]) #difference between high and low
-            
+            inputs.append(btc_evol[k])
             seq_x.append(inputs)
 
 
